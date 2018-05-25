6aee4ad7adc94965a150dd0ff3bf8e3908ee15cf
Yohannes Abera <yohannesns@gmail.com>
2018-05-25 02:27:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 function happyHolidays(){
-  return "Happy Holidays!"
+  return "Happy holidays!"
 }
-// function happyHolidaysTo(name){
-//   return "Happy Holidays",`${name}!`
-// }
+function happyHolidaysTo(name){
+  return "Happy holidays",`${name}!`
+}
 // function happyHolidayTo(holiday,name){
 //   return "Happy" `${holiday}`,`${name}`
 // }
